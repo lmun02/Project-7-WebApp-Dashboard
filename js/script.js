6e38abb48f8357119d0ce63a9f38070e6323ff85
Lia Mun <liacmun@gmail.com>
2020-05-12 13:40:35 -0700

    CSS updates, HTML changes
    updating CSS layout, html changes

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const send = document.getElementById("send");
 alertBanner.innerHTML =
     `
     <div class="alert-banner">
-    <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+    <p><strong>Alert</strong> You have <strong>6</strong> overdue tasks to complete</p>
     <p class="alert-banner-close">x</p>
     </div>
     `
